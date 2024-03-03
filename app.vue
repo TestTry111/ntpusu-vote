ad2dd61c2bf49f167984fbc3463f3d55077278e8
SuAnn1103 <91890086+SuAnn1103@users.noreply.github.com>
2024-03-03 23:50:46 +0800

    test: google登入

--- app.vue
+++ app.vue
@@ -393,12 +393,13 @@ const checkLogin = () => {
     setTimeout(async () => {
         if (status.value) {
             if (status.value === 'authenticated') {
+                /*
                 const {
                     data: res,
                     refresh,
                     error,
                 } = await useFetch('/api/checkLogin')
-
+                
                 while (!res.value) {
                     if (error.value) {
                         ElMessage.error('操作過於頻繁，無法獲得登入序號')
@@ -409,13 +410,14 @@ const checkLogin = () => {
 
                     setTimeout(() => {}, 500)
                 }
-
-                loginInfo.value = res.value.login
+                */
+                //loginInfo.value = res.value.login
                 showLoginBadge.value = true
-
+                /*
                 if (res.value.firstLogin) {
                     await showLoginInfo()
                 }
+                */
             }
         } else checkLogin()
     }, 250)
